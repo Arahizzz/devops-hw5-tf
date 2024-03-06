3acf77a1ed4f37a433fa564f6e5d9c884d6d0721
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2024-03-07 01:05:02 +0200

    Change instance type to nano

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "Polishchuk-tf-instance"
+  name = "Polishchuk-tf-instance-nano"
 
-  instance_type          = "t3.micro"
+  instance_type          = "t3.nano"
   key_name               = "PolishchukKey"
   monitoring             = true
   vpc_security_group_ids = ["sg-0d4c31d951154bcf1"]
