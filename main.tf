512a28e06f9752a07cf2a20674f84e2688aeb6f9
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2024-03-07 01:00:33 +0200

    Init instance

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "Polishchuk-tf-instance"
+
+  instance_type          = "t3.micro"
+  key_name               = "PolishchukKey"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0d4c31d951154bcf1"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
